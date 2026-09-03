fc0c9ca846e61481e12a470d0e20c6f5a971551a
John Gallie <jtgsystems@gmail.com>
2026-09-03 01:11:20 -0400

    Update SEO optimized project descriptions, package metadata, and PyPI classifiers

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="surround-sound-monitor-setup",
     version="1.0.0",
+    description="Turn multi-monitor battlestations into 5.1/7.1 Spatial Surround Sound or Unified Wide Stereo arrays using computer screens as speakers. Automatic PipeWire & PulseAudio calibrator for Linux.",
+    author="JTG Systems",
+    author_email="jtgsystems@gmail.com",
+    url="https://jtgsystems.com",
     packages=find_packages(),
     install_requires=[
         "rich>=13.0.0",
